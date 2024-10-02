af524316832f1618013adcde7597783b4e2b0959
Stefan Schlosser <bsdcode@disroot.org>
2024-10-02 12:31:31 +0200

    thirdparty/librashader: activate runtimes in librashader_ld.h

--- thirdparty/librashader/include/librashader_ld.h
+++ thirdparty/librashader/include/librashader_ld.h
@@ -29,17 +29,17 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 // Uncomment the following defines to activate runtimes.
 
-// #define LIBRA_RUNTIME_OPENGL
+#define LIBRA_RUNTIME_OPENGL
 // #define LIBRA_RUNTIME_VULKAN
 
-// #if defined(_WIN32)
-// #define LIBRA_RUNTIME_D3D11
-// #define LIBRA_RUNTIME_D3D12
-// #endif
+#if defined(_WIN32)
+#define LIBRA_RUNTIME_D3D11
+#define LIBRA_RUNTIME_D3D12
+#endif
 
-// #if (defined(__APPLE__) && defined(__OBJC__))
-// #define LIBRA_RUNTIME_METAL
-// #endif
+#if (defined(__APPLE__) && defined(__OBJC__))
+#define LIBRA_RUNTIME_METAL
+#endif
 
 #if defined(_WIN32)
 #include <windows.h>
